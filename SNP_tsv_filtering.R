068829b58d21eeed01cbb993e892e8a3eb0ef509
Turgut Yigit Akyol tya@mbg.au.dk <tyakyol@fe1.genomedk.net>
2020-06-30 22:58:07 +0200

    HOB filtering was added.

--- SNP_tsv_filtering.R
+++ SNP_tsv_filtering.R
@@ -11,22 +11,27 @@ snps = read.delim(input,
 # Remove columns with all NA
 not_any_na = function(x) all(!is.na(x))
 snps = select_if(snps, not_any_na)
+for(i in 3:6) {
+  snps[, i] = as.numeric(snps[, i])
+}
 # Gifu is either 0/0 or 0/1
-snps = snps[snps$X.6.results.sorted_Gifu_R.bam.GT %in% c('0/0', '0/1'), ]
+snps = snps[snps$X.7.results.sorted_Gifu_R.bam.GT %in% c('0/0', '0/1'), ]
 # MQ >= 40
 snps = snps[snps$X.5.MQ >= 40, ]
 # DP >= 200
 snps = snps[snps$X.4.DP >= 200, ]
+# HOB <= 0.05
+snps = snps[snps$X.6.HOB <= 0.05, ]
 # Missing < 50%
-missing = apply(snps[, 6:166], 1, function(x) sum(x == './.'))
+missing = apply(snps[, 7:167], 1, function(x) sum(x == './.'))
 snps = snps[missing <= 81, ]
 # Must be biallelic
-biallelic = apply(snps[, 6:166], 1, function(x) names(table(x)))
+biallelic = apply(snps[, 7:167], 1, function(x) names(table(x)))
 biallelic = unlist(biallelic)
 biallelic = levels(factor(biallelic))
 biallelic = biallelic[!(biallelic %in% c('./.', '0/0', '0/1', '1/1'))]
 biallelic = (apply(snps, 1, function(x) sum(!(x %in% biallelic))))
-snps = snps[biallelic == 166, ]
+snps = snps[biallelic == 167, ]
 positions = snps[, 1:2, drop = FALSE]
 # Output
 write.table(positions, file = output,
